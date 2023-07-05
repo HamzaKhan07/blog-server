f18ae6bbe27b70bd602bacac6da2310d2460b454
hamza <hamzakhan48208@gmail.com>
2023-07-05 09:57:01 +0530

    changed index

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 mongoose.connect('mongodb+srv://hamzakhan48208:bXAbjJKrmQ4Wc0l4@cluster0.urcj6v8.mongodb.net/?retryWrites=true&w=majority');
 
 // for cors
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 
 app.get('/', function(req, res){
